53b7af650012e8a4eeac9d6c3f83e7feed21622b
Skyler Adams <skyleradams@nimbus.local>
2014-11-30 23:40:06 -0500

    python trajectory tracking

--- Python/dynamixel_control.py
+++ Python/dynamixel_control.py
@@ -5,6 +5,8 @@ import options
 import math
 import serial
 
+ticks_per_rad = 4096.0/(math.pi*2)
+
 ############################################
 #  _______         __ ______                     __
 # /_  __(_)_ _    / // / __ \_    _____ ________/ /
@@ -21,22 +23,174 @@ def forwardKinematics(theta1, theta2, l1, l2):
 #Given: xE,yE, l1, l2
 #Return: theta1,theta2
 def inverseKinematics(xIn, yIn, l1, l2):
-    myTheta2 = 2*math.atan2(math.sqrt(((l1+l2)**2-(xIn**2+yIn**2))),math.sqrt((xIn**2+yIn**2.0)-(l1-l2)**2))
-    myTheta1 = math.atan2(yIn,xIn)-math.atan2(l2*math.sin(myTheta2),l1+l2*math.cos(myTheta2))
-    return (myTheta1, myTheta2)
+	myTheta2 = 2*math.atan2(math.sqrt(((l1+l2)**2-(xIn**2+yIn**2))),math.sqrt((xIn**2+yIn**2.0)-(l1-l2)**2))
+	myTheta1 = math.atan2(yIn,xIn)-math.atan2(l2*math.sin(myTheta2),l1+l2*math.cos(myTheta2))
+	return (scaleToCircle(myTheta1), scaleToCircle(myTheta2))
 
-def initializeDynamixels():
-	if os.name == "posix":
-	    portName = options.unix_port
+def computeAltIK(x, y, theta1, theta2):
+	#theta1 and 2 are IK outputs
+	t2 = -theta2
+	angle_to_endpoint = scaleToCircle(math.atan2(y,x))
+	if angle_to_endpoint > theta1:
+		t1 = theta1 + 2*(angle_to_endpoint-theta1)
+	elif angle_to_endpoint < theta1:
+		t1 = theta1 + 2*(angle_to_endpoint-theta1)
 	else:
-	    portName = options.windows_port
+		t1 = theta1
+
+	return (t1, t2)
+
+def scaleToCircle(radianvalue):
+	return radianvalue % (2*math.pi)
 
 
-	print "Scanning for Dynamixels...",
-	for dyn in net.get_dynamixels():
-	    print dyn.id,
-	    myActuators.append(net[dyn.id])
-	print "...Done"
+def boundWithinGoal(value, upper, lower):
+	if value > upper:
+		bounded = upper
+	elif value < lower:
+		bounded = lower
+	else:
+		bounded = value
+	return bounded
+
+def boundWithinRobotReach(x, y, radius):
+	if math.sqrt(math.pow(x,2)+math.pow(y,2)) > radius:
+		angle = math.atan2(y,x)
+		return (radius*math.cos(angle), radius*math.sin(angle))
+	else:
+		return (x,y)
+def withinThreshold(difference, thresh):
+	if abs(difference) <= thresh:
+		return True
+	elif abs(abs(difference)-2*math.pi) <= thresh:
+		return True
+	else:
+		return False
+
+def actuatorsMoving(actuators):
+    for actuator in actuators:
+        if actuator.cache[dynamixel.defs.REGISTER['Moving']]:
+            return True
+    return False
+
+
+if os.name == "posix":
+    portName = options.unix_port
+else:
+    portName = options.windows_port
+
+serial = dynamixel.serial_stream.SerialStream( port=portName, baudrate=options.baudrate, timeout=1)
+net = dynamixel.dynamixel_network.DynamixelNetwork( serial )
+net.scan( 1, options.num_servos )
+
+print "Scanning for Dynamixels...",
+for dyn in net.get_dynamixels():
+    print dyn.id,
+    myActuators.append(net[dyn.id])
+
+print "FOUND:" + str(myActuators)
+
+for actuator in myActuators:
+	actuator.moving_speed = options.servo_speed
+	actuator.synchronized = True
+	actuator.torque_enable = True
+	actuator.torque_control_enable = False
+	actuator.torque_limit = 1024
+	actuator.max_torque = 1024
+
+
+
+
+
+
+
+class Arm(object):
+
+	def __init__(self, shoulder, elbow, params):
+		self.params = params
+		self.shoulder = shoulder
+		self.elbow = elbow
+		self.elbow_angle = 0
+		self.shoulder_angle = 0
+		#motors
+
+	def update(self):
+		net.synchronize()
+		self.shoulder.read_all()
+		self.elbow.read_all()
+
+	def moveToXY(self,x,y):
+		theta1, theta2 = inverseKinematics(x,y, self.params.l1, self.params.l2)
+		alt_theta1, alt_theta2 = computeAltIK(x,y, theta1,theta2)
+		if (abs(self.shoulder_angle - theta1)+abs(self.elbow_angle-theta2)) < (abs(self.shoulder_angle - alt_theta1)+abs(self.elbow_angle-alt_theta2)/2):
+			self.moveToTheta(theta1, theta2)
+		else:
+			self.moveToTheta(alt_theta1, alt_theta2)
+
+
+	def moveToXYGoal(self, x, y):
+		x, y = Arm.transformGoaltoRobot(self,x,y)
+		x, y = boundWithinRobotReach(x,y, self.params.l1+self.params.l2)
+		x = boundWithinGoal(x, self.params.max_x, self.params.min_x)
+		y = boundWithinGoal(y, self.params.max_y, self.params.min_y)
+		self.moveToXY(x,y)
+
+
+	def transformGoaltoRobot(self,x,y):
+		return (x-self.params.horizontal_offset, y-self.params.vertical_offset)
+
+
+	def moveToTheta(self, t1, t2):
+		print t1, t2
+		self.shoulder_angle = scaleToCircle(t1)
+		self.elbow_angle = scaleToCircle(t2)
+		self.shoulder.goal_position = int((self.shoulder_angle*ticks_per_rad)+self.params.shoulder_offset)
+		self.elbow.goal_position = int(((self.elbow_angle*ticks_per_rad) +self.params.elbow_offset)/2)
+
+
+	def isMoving(self):
+	    for actuator in [self.shoulder, self.elbow]:
+	        if actuator.cache[dynamixel.defs.REGISTER['Moving']]:
+	            return True
+	    return False
+
+
+	def returnCurrentPositions(self):
+		theta1 = (self.shoulder.cache[dynamixel.defs.REGISTER['CurrentPosition']]-self.params.shoulder_offset)/ticks_per_rad
+		theta2 = (self.elbow.cache[dynamixel.defs.REGISTER['CurrentPosition']]-self.params.elbow_offset)/ticks_per_rad*2
+		theta1 = scaleToCircle(theta1)
+		theta2 = scaleToCircle(theta2)
+		return (theta1, theta2)
+
+	def nearGoalPosition(self):
+		shoulder, elbow = Arm.returnCurrentPositions(self)
+		if withinThreshold(scaleToCircle(shoulder-self.shoulder_angle),self.params.angle_threshold) and withinThreshold(scaleToCircle(elbow-self.elbow_angle),self.params.angle_threshold):
+			return True
+		else:
+			return False
+		
+
+
+
+a = Arm(myActuators[0], myActuators[1], options.left_arm)
+
+trajectories = [(30, 18.5),(20, 24) ,(16, 30)]
+
+a.moveToXYGoal(trajectories[0][0], trajectories[0][1])
+
+while True:
+	a.update()
+	if a.nearGoalPosition():
+		trajectories.pop(0)
+		a.moveToXYGoal(trajectories[0][0], trajectories[0][1])
+	
+
+
+
+
+
+
+
 
 
 
--- Python/options.py
+++ Python/options.py
@@ -1,19 +1,47 @@
 # measurements in inches
-arm_vertical_offset = 18.5
-left_horizontal_offset = 14.5
-right_horizontal_offset = 43.5
-l1 = 11
-l2 = 11
-#ticks 
-left_shoulder_offset = 0
-left_elbow_offset = -50
-right_shoulder_offset = 0
-right_elbow_offset = 0
+ball_radius = 3
+goal_top = 50
+goal_width = 58
+goal_half = 29
 
-move_speed = 50
-baudrate = 400000
+angle_threshold = .1
+
+
+
+class L_params(object):
+	horizontal_offset = 14.5
+	vertical_offset = 18.5
+	min_y = ball_radius - vertical_offset # in robot coords
+	max_y = goal_top - vertical_offset
+	min_x = -14.5
+	max_x = 14.5
+	l1 = 11
+	l2 = 11
+	shoulder_offset = 0
+	elbow_offset = -50
+	angle_threshold = angle_threshold
+
+
+class R_params(object):
+	horizontal_offset = 43.5
+	vertical_offset = 18.5
+	min_y = ball_radius - vertical_offset # in robot coords
+	max_y = goal_top - vertical_offset
+	min_x = -14.5
+	max_x = 14.5
+	l1 = 11
+	l2 = 11
+	shoulder_offset = 0
+	elbow_offset = 0
+	angle_threshold = angle_threshold
+
+left_arm = L_params()
+right_arm = R_params()
 
 windows_port = "COM8"
 unix_port = "/dev/tty.usbserial-A4012933"
 
+num_servos = 2
+servo_speed = 300
+baudrate = 400000
 
